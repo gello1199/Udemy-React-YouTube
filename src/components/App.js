fc1a19420f8d36a9faf1c643372c1b184da8b01b
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-06 13:43:44 -0500

    Refactor onVideoSelect to inline function

--- src/components/App.js
+++ src/components/App.js
@@ -22,10 +22,6 @@ const App = () => {
         setSelectedVideo(response.data.items[0]) 
     }
 
-    const onVideoSelect = (video) => {
-        setSelectedVideo(video)
-    }
-
     return (
         <div className="ui container">
             <SearchBar onTermSubmit={onTermSubmit} />
@@ -36,7 +32,7 @@ const App = () => {
                     </div>
                     <div className="five wide column">
                         <VideoList 
-                        onVideoSelect={onVideoSelect} 
+                        onVideoSelect={setSelectedVideo} 
                         videos={videos} 
                         />
                     </div>
