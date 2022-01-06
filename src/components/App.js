85b671cafd0a071136a20953a25007adc6792bf0
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-06 14:10:02 -0500

    Add useVideos custom hook

--- src/components/App.js
+++ src/components/App.js
@@ -5,22 +5,10 @@ import VideoList from './VideoList';
 import Videodetail from './VideoDetail';
 
 const App = () => {
-    const [videos, setVideos] = useState([])
+   
     const [selectedVideo, setSelectedVideo] = useState(null)
 
-    useEffect(() => {
-        onTermSubmit('hockey')
-    }, [])
-
-    const onTermSubmit = async term => {
-        const response = await youtube.get('/search', {
-            params: {
-                q: term
-            }
-        })
-        setVideos(response.data.items)
-        setSelectedVideo(response.data.items[0]) 
-    }
+    // setSelectedVideo(response.data.items[0])
 
     return (
         <div className="ui container">
